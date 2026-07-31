80784bdddf0a87810849ba0106a15f511f274ffe
RazvanButnaruAlfen <razvan.butnaru@alfen.com>
2026-08-01 01:41:59 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from components.mission_map import render_mission_map
 from components.timeline import render_timeline
 from components.cards import render_cards
 from components.footer import render_footer
+from components.funny_panel import render_funny_panel
 from core.mission_engine import get_mission_state
 
 st.set_page_config(
@@ -17,4 +18,5 @@ render_header()
 render_mission_map(state)
 render_timeline(state)
 render_cards(state)
+render_funny_panel(state)
 render_footer()
