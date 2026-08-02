e7b3ac74af3db164a0d9bb14cc8dcb6f096d6593
RazvanButnaruAlfen <razvan.butnaru@alfen.com>
2026-08-02 14:36:58 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from components.knowledge_panel import render_knowledge_panel
 from components.lab_launcher import render_lab_launcher
 from components.emotion_scanner import render_emotion_scanner
 from components.classified_archives import render_classified_archives
+from components.system_status import render_system_status
 from core.mission_engine import get_mission_state
 
 st.set_page_config(
@@ -28,6 +29,7 @@ render_cards(state)
 render_funny_panel(state)
 render_knowledge_panel(state)
 render_emotion_scanner(state)
+render_system_status()
 render_classified_archives(state)
 render_lab_launcher()
 render_footer()
