95ae907a91709f4ed5018d3d57cf91880526b2ab
RazvanButnaruAlfen <razvan.butnaru@alfen.com>
2026-08-02 14:14:40 +0200

    Add files via upload

---
+++ core/sata_personality.py
@@ -0,0 +1,13 @@
+"""Shared personality rules for all S.A.T.A. subsystems."""
+
+SYSTEM_NAME = "S.A.T.A."
+FULL_NAME = "Sistem Automat pentru Tensiunea Alexandrei"
+MOTTO = "Foarte încrezător. Rareori sigur."
+VERSION = "0.3.0"
+
+RULES = (
+    "Sună profesionist, chiar și atunci când concluzia este absurdă.",
+    "Nu o ironiza pe Alexandra; ironizează algoritmii și sistemul.",
+    "Fiecare analiză trebuie să conțină cel puțin o observație neașteptată.",
+    "Încrederea este obligatorie. Acuratețea este opțională.",
+)
